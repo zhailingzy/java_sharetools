aca35928fc50df1216c2fdca74dd68aa0d5e4a64
zhailingzy <zhailingzy@gmail.com>
2015-10-21 12:28:35 +0800

    modify throw logif for inictrl

--- src/ShareTools/IniCtrl.java
+++ src/ShareTools/IniCtrl.java
@@ -12,54 +12,86 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Properties;
 
+import javax.swing.JOptionPane;
+
 public class IniCtrl {
 	private static Properties p;
 	private String inifile;
+	private MessageBox msgBox;
+	private String s_errTitle = "INI Control Error";
+	private boolean showError;
 
-	public IniCtrl(String inipath, Properties defaultpro) throws FileNotFoundException, IOException {
-
+	public IniCtrl(String inipath, Properties defaultpro,boolean showError) throws IOException {
+		msgBox = new MessageBox();
+		this.showError = showError;
 		if (!Files.isReadable(Paths.get(inipath))) {
-			Files.createFile(Paths.get(inipath));
+			try {
+				Files.createFile(Paths.get(inipath));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				if (showError)
+					msgBox.showMessage(s_errTitle , e.getMessage(), JOptionPane.ERROR_MESSAGE);
+				throw e;
+			}
 			p = new Properties(defaultpro);
 			p.putAll(defaultpro);
 		} else {
 			p = new Properties(defaultpro);
 		}
-		p.load(new FileInputStream(inipath));
-		inifile = inipath;
-	}
-
-	public void save() {
 		try {
-			p.store(new FileOutputStream(inifile), null);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			p.load(new FileInputStream(inipath));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			if (showError)
+				msgBox.showMessage(s_errTitle , e.getMessage(), JOptionPane.ERROR_MESSAGE);
+			throw e;
 		}
+		inifile = inipath;
+	}
+
+	public void save() throws IOException {
+			try {
+				p.store(new FileOutputStream(inifile), null);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				if (showError)
+					msgBox.showMessage(s_errTitle , e.getMessage(), JOptionPane.ERROR_MESSAGE);
+				throw e;
+			}
+
 	}
 
 	@Override
-	protected void finalize() throws Throwable {
+	protected void finalize() throws Throwable  {
 		// TODO Auto-generated method stub
-		super.finalize();
 		save();
+		try {
+			super.finalize();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			if (showError)
+				msgBox.showMessage(s_errTitle , e.getMessage(), JOptionPane.ERROR_MESSAGE);
+			throw e;
+		}
+		
 	}
 
-	public void defaultIniSetting(String[] key, String[] value) {
+	public void defaultIniSetting(String[] key, String[] value) throws IOException {
 		for (int i = 0; i < key.length; i++) {
 			p.setProperty(key[i], value[i]);
 		}
 		save();
 	}
 
-	public String getIniSetting(String key) throws FileNotFoundException, IOException {
+	public String getIniSetting(String key) {
 		return p.getProperty(key);
 	}
 
-	public void setIniSetting(String key, String value) {
+	public void setIniSetting(String key, String value) throws IOException {
 		p.setProperty(key, value);
 		save();
 
